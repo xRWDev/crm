69866c63787b84d74bf5e5c970ce7a15a88f0212
xrwdev <xqvens@gmail.com>
2026-02-07 12:49:38 +0200

    Reduce global corner radius

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+document.documentElement.classList.add("soft-ui");
+
 createRoot(document.getElementById("root")!).render(<App />);
